f3a21550bad7ec3c211851840bbf62943db273b8
tasneemakhaled <khaledtasneem402@gmail.com>
2026-03-03 23:38:04 +0200

    add assets of onboarding view

--- lib/feartures/onboarding/presentation/views/onboarding_view.dart
+++ lib/feartures/onboarding/presentation/views/onboarding_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/feartures/onboarding/presentation/views/widgets/onboarding_view_body.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
 static const routeName="onboarding";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: OnboardingViewBody(),
+    );
   }
 }
