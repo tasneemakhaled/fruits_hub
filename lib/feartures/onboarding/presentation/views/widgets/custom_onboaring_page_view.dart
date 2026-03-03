8fbe5fd5e36d9fa2003a3e077fe6a608404c00c1
tasneemakhaled <khaledtasneem402@gmail.com>
2026-03-03 23:50:03 +0200

    create page view

---
+++ lib/feartures/onboarding/presentation/views/widgets/custom_onboaring_page_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomOnboaringPageView extends StatelessWidget {
+  const CustomOnboaringPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        
+      ],);
+  }
+}
--- lib/feartures/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/feartures/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/feartures/onboarding/presentation/views/widgets/custom_onboaring_page_view.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Expanded(child: CustomOnboaringPageView()),
+      ],
+    );
   }
 }
