920ff6cf2874b7cc61db920aae75aacf0156e832
tasneemakhaled <khaledtasneem402@gmail.com>
2026-04-10 19:24:04 +0200

    implement create user with email and password

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,4 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/exceptions.dart';
+
+
 class FirebaseAuthService {
 
-  
+  static Future<User>createUserWithEmainAndPassword({required String email,required String password })async{
+
+    try {
+  final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+  return credential.user!;
+} on FirebaseAuthException catch (e) {
+  if (e.code == 'weak-password') {
+    throw CustomException(message: 'The password provided is too weak.');
+    // print('The password provided is too weak.');
+  } else if (e.code == 'email-already-in-use') {
+   throw CustomException(message: 'The account already exists for that email.');
+    // print('The account already exists for that email.');
+  }else{
+      throw CustomException(message: 'there was an error pls try later');
+  }
+} catch (e) {
+  throw CustomException(message: 'there was an error pls try later');
+}
+  }
 }
--- lib/feartures/auth/data/repos/auth_repo_impl.dart
+++ lib/feartures/auth/data/repos/auth_repo_impl.dart
@@ -1,12 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:fruits_hub/core/errors/failure.dart';
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
 import 'package:fruits_hub/feartures/auth/domain/entities/user_entity.dart';
 import 'package:fruits_hub/feartures/auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo{
   @override
   Future<Either<Failure, UserEntity>> createUserWithEmainAndPassword(String email, String password) {
-    // TODO: implement createUserWithEmainAndPassword
+    FirebaseAuthService.createUserWithEmainAndPassword(email: email, password: password);
+  
     throw UnimplementedError();
   }
 }
