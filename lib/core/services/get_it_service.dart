c53953f82b9a60349680d2417e90e17aa12400be
tasneemakhaled <khaledtasneem402@gmail.com>
2026-04-11 22:52:03 +0200

    add get it service locator and provide cubit to sign up view done

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,10 @@
+import 'package:fruits_hub/core/services/firebase_auth_service.dart';
+import 'package:fruits_hub/feartures/auth/data/repos/auth_repo_impl.dart';
+import 'package:fruits_hub/feartures/auth/domain/repos/auth_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+void setUp(){
+  getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<AuthRepo>(AuthRepoImpl(firebaseAuthService: getIt<FirebaseAuthService>()));
+}
--- lib/feartures/auth/presentation/views/sign_up_view.dart
+++ lib/feartures/auth/presentation/views/sign_up_view.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/feartures/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/feartures/auth/presentation/view_models/cubits/SignUp/sign_up_cubit.dart';
 import 'package:fruits_hub/feartures/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
-static const routeName='signUp';
+  static const routeName = 'signUp';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: (){},
-         icon: Icon( Icons.arrow_back_ios_new)),
-        title: Text('حساب جديد',style: TextStyles.bold19,),
+    return BlocProvider(
+      create: (context) => SignUpCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.arrow_back_ios_new),
+          ),
+          title: Text('حساب جديد', style: TextStyles.bold19),
+        ),
+        body: SignUpViewBody(),
       ),
-      body: SignUpViewBody(),
     );
   }
 }
