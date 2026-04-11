4a4494daa96ecf6e84e8540b713fba0f5c6bee41
tasneemakhaled <khaledtasneem402@gmail.com>
2026-04-11 21:57:18 +0200

    sign up cubit done

---
+++ lib/feartures/auth/presentation/view_models/cubits/SignUp/sign_up_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_hub/feartures/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/feartures/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit(this.authRepo) : super(SignUpInitial());
+ final AuthRepo authRepo;
+ Future<void>createUserWithEmailAndPassword(String email,String password,String name)async{
+  emit(SignUpLoading());
+  final result=await authRepo.createUserWithEmainAndPassword(email, password, name);
+  result.fold(
+    (failue){
+      emit(SignUpFailure(message: failue.errMessage));
+    }, 
+    (userEntity){
+      emit(SignUpSuccess(userEntity: userEntity));
+    });
+ }
+}
---
+++ lib/feartures/auth/presentation/view_models/cubits/SignUp/sign_up_state.dart
@@ -0,0 +1,17 @@
+part of 'sign_up_cubit.dart';
+
+@immutable
+sealed class SignUpState {}
+
+final class SignUpInitial extends SignUpState {}
+final class SignUpLoading extends SignUpState {}
+final class SignUpSuccess extends SignUpState {
+  final UserEntity userEntity;
+
+  SignUpSuccess({required this.userEntity});
+}
+final class SignUpFailure extends SignUpState {
+  final String message;
+
+  SignUpFailure({required this.message});
+}
