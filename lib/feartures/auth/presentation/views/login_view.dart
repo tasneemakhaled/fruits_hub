5e39ceffd094f6658b698531c56a7206dacfb891
tasneemakhaled <khaledtasneem402@gmail.com>
2026-04-20 21:22:46 +0200

    auth feature done

--- lib/feartures/auth/presentation/views/login_view.dart
+++ lib/feartures/auth/presentation/views/login_view.dart
@@ -5,7 +5,6 @@ import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/feartures/auth/domain/repos/auth_repo.dart';
 import 'package:fruits_hub/feartures/auth/presentation/view_models/cubits/LogIn/login_cubit.dart';
 import 'package:fruits_hub/feartures/auth/presentation/views/widgets/bloc_consumer_login_view_body.dart';
-import 'package:fruits_hub/feartures/auth/presentation/views/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
--- lib/feartures/auth/presentation/views/widgets/bloc_consumer_signup_view_body.dart
+++ lib/feartures/auth/presentation/views/widgets/bloc_consumer_signup_view_body.dart
@@ -4,7 +4,6 @@ import 'package:fruits_hub/core/helper_functions/build_error_bar.dart';
 import 'package:fruits_hub/feartures/auth/presentation/view_models/cubits/SignUp/sign_up_cubit.dart';
 import 'package:fruits_hub/core/widgets/custom_prgress_hud.dart';
 import 'package:fruits_hub/feartures/auth/presentation/views/widgets/sign_up_view_body.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class BlocConsumerSignupViewBody extends StatelessWidget {
   const BlocConsumerSignupViewBody({super.key});
