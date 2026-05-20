8330bf38154d1c2c502f7a129d82c59fe86a9e82
tasneemakhaled <khaledtasneem402@gmail.com>
2026-05-20 15:09:01 +0300

    select shipping item

--- lib/feartures/checkout/presentation/views/shipping_view.dart
+++ lib/feartures/checkout/presentation/views/shipping_view.dart
@@ -9,12 +9,14 @@ class ShippingView extends StatelessWidget {
     return Column(
       children: [
         ShippingItem(
+          isSelected: true,
           title: 'الدفع عند الاستلام',
           subTitle: 'التسليم من المكان',
           price: '40 جنيه',
         ),
         SizedBox(height: 16),
         ShippingItem(
+          isSelected: false,
           title: 'اشتري الان وادفع لاحقا',
           subTitle: 'يرجي تحديد طريقه الدفع',
           price: 'مجاني',
--- lib/feartures/checkout/presentation/views/widgets/shipping_item.dart
+++ lib/feartures/checkout/presentation/views/widgets/shipping_item.dart
@@ -1,44 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/feartures/checkout/presentation/views/widgets/checkout_steps_list_view.dart';
 
-class ShippingItem extends StatelessWidget {
+class ShippingItem extends StatefulWidget {
   const ShippingItem({
     super.key,
     required this.title,
     required this.subTitle,
     required this.price,
+    required this.isSelected,
   });
   final String title, subTitle, price;
+  final bool isSelected;
+  @override
+  State<ShippingItem> createState() => _ShippingItemState();
+}
+
+class _ShippingItemState extends State<ShippingItem> {
+  bool isClicked = false;
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xfff9fafa),
-      child: ListTile(
-        leading: Container(
-          decoration: ShapeDecoration(
-            color: Color(0xff949d9e),
-            shape: RoundedRectangleBorder(
-              side: BorderSide(width: 1, color: Color(0xff949d9e)),
-              borderRadius: BorderRadius.circular(18),
-            ),
+      decoration: ShapeDecoration(
+        color: Color(0xfff9fafa),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: widget.isSelected
+                ? AppColors.darkPrimaryColor
+                : Colors.transparent,
           ),
-          child: CircleAvatar(
-            radius: 9,
-            backgroundColor: Colors.white,
+        ),
+      ),
+      child: ListTile(
+        leading: GestureDetector(
+          onTap: () {
+            isClicked = true;
+            setState(() {});
+          },
+          child: Container(
+            decoration: ShapeDecoration(
+              color: Color(0xff949d9e),
+              shape: RoundedRectangleBorder(
+                side: widget.isSelected
+                    ? BorderSide.none
+                    : BorderSide(width: .5, color: Color(0xff949d9e)),
+                borderRadius: BorderRadius.circular(18),
+              ),
+            ),
             child: CircleAvatar(
-              radius: 4.5,
-              backgroundColor: AppColors.darkPrimaryColor,
+              radius: widget.isSelected ? 12 : 9,
+              backgroundColor: Colors.white,
+              child: widget.isSelected
+                  ? CircleAvatar(
+                      radius: 6,
+                      backgroundColor: AppColors.darkPrimaryColor,
+                    )
+                  : SizedBox(),
             ),
           ),
         ),
-        title: Text(title, style: AppStyles.semiBold13),
+        title: Text(widget.title, style: AppStyles.semiBold13),
         subtitle: Text(
-          subTitle,
+          widget.subTitle,
           style: AppStyles.regular13.copyWith(color: Color(0xff8c8c8c)),
         ),
         trailing: Text(
-          price,
+          widget.price,
           style: AppStyles.bold13.copyWith(color: Color(0xff3A8B33)),
         ),
       ),
