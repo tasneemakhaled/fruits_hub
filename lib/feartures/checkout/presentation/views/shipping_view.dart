957f9425897e12cc465716754bee7079dbfc735c
tasneemakhaled <khaledtasneem402@gmail.com>
2026-05-20 15:22:50 +0300

    shipping ui done

--- lib/feartures/checkout/presentation/views/shipping_view.dart
+++ lib/feartures/checkout/presentation/views/shipping_view.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/feartures/checkout/presentation/views/widgets/shipping_item.dart';
 
-class ShippingView extends StatelessWidget {
+class ShippingView extends StatefulWidget {
   const ShippingView({super.key});
 
+  @override
+  State<ShippingView> createState() => _ShippingViewState();
+}
+
+int selectedIndex = -1;
+
+class _ShippingViewState extends State<ShippingView> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         ShippingItem(
-          isSelected: true,
+          onTap: () {
+            selectedIndex = 0;
+            setState(() {});
+          },
+          isSelected: selectedIndex == 0,
           title: 'الدفع عند الاستلام',
           subTitle: 'التسليم من المكان',
           price: '40 جنيه',
         ),
         SizedBox(height: 16),
         ShippingItem(
-          isSelected: false,
+          onTap: () {
+            selectedIndex = 1;
+            setState(() {});
+          },
+          isSelected: selectedIndex == 1,
           title: 'اشتري الان وادفع لاحقا',
           subTitle: 'يرجي تحديد طريقه الدفع',
           price: 'مجاني',
--- lib/feartures/checkout/presentation/views/widgets/shipping_item.dart
+++ lib/feartures/checkout/presentation/views/widgets/shipping_item.dart
@@ -1,57 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:fruits_hub/feartures/checkout/presentation/views/widgets/checkout_steps_list_view.dart';
 
-class ShippingItem extends StatefulWidget {
+class ShippingItem extends StatelessWidget {
   const ShippingItem({
     super.key,
     required this.title,
     required this.subTitle,
     required this.price,
     required this.isSelected,
+    this.onTap,
   });
   final String title, subTitle, price;
   final bool isSelected;
-  @override
-  State<ShippingItem> createState() => _ShippingItemState();
-}
-
-class _ShippingItemState extends State<ShippingItem> {
-  bool isClicked = false;
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return AnimatedContainer(
+      duration: Duration(microseconds: 300),
       decoration: ShapeDecoration(
         color: Color(0xfff9fafa),
         shape: RoundedRectangleBorder(
           side: BorderSide(
-            color: widget.isSelected
-                ? AppColors.darkPrimaryColor
-                : Colors.transparent,
+            color: isSelected ? AppColors.darkPrimaryColor : Colors.transparent,
           ),
         ),
       ),
       child: ListTile(
         leading: GestureDetector(
-          onTap: () {
-            isClicked = true;
-            setState(() {});
-          },
+          onTap: onTap,
           child: Container(
             decoration: ShapeDecoration(
               color: Color(0xff949d9e),
               shape: RoundedRectangleBorder(
-                side: widget.isSelected
+                side: isSelected
                     ? BorderSide.none
                     : BorderSide(width: .5, color: Color(0xff949d9e)),
                 borderRadius: BorderRadius.circular(18),
               ),
             ),
             child: CircleAvatar(
-              radius: widget.isSelected ? 12 : 9,
+              radius: isSelected ? 12 : 9,
               backgroundColor: Colors.white,
-              child: widget.isSelected
+              child: isSelected
                   ? CircleAvatar(
                       radius: 6,
                       backgroundColor: AppColors.darkPrimaryColor,
@@ -60,13 +51,13 @@ class _ShippingItemState extends State<ShippingItem> {
             ),
           ),
         ),
-        title: Text(widget.title, style: AppStyles.semiBold13),
+        title: Text(title, style: AppStyles.semiBold13),
         subtitle: Text(
-          widget.subTitle,
+          subTitle,
           style: AppStyles.regular13.copyWith(color: Color(0xff8c8c8c)),
         ),
         trailing: Text(
-          widget.price,
+          price,
           style: AppStyles.bold13.copyWith(color: Color(0xff3A8B33)),
         ),
       ),
