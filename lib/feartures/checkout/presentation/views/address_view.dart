8207b1d604842cecb011f5f472091db44b15dc88
tasneemakhaled <khaledtasneem402@gmail.com>
2026-05-20 15:32:32 +0300

    address input ui done

--- lib/feartures/checkout/presentation/views/address_view.dart
+++ lib/feartures/checkout/presentation/views/address_view.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/feartures/auth/presentation/views/widgets/custom_text_field.dart';
 
 class AddressView extends StatelessWidget {
   const AddressView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 16),
+          CustomTextField(hintText: 'الاسم كامل'),
+          SizedBox(height: 8),
+          CustomTextField(hintText: ' البريد الإلكتروني'),
+          SizedBox(height: 8),
+          CustomTextField(hintText: 'العنوان'),
+          SizedBox(height: 8),
+          CustomTextField(hintText: 'المدينه'),
+          SizedBox(height: 8),
+          CustomTextField(hintText: 'رقم الطابق , رقم الشقه ..'),
+          SizedBox(height: 8),
+          CustomTextField(hintText: 'رقم الهاتف'),
+          SizedBox(height: 8),
+        ],
+      ),
+    );
   }
 }
